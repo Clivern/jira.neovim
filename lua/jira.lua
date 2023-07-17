773e6058c03f89721c3313750b0a5d1a85a979ff
Clivern <hello@clivern.com>
2023-07-17 23:27:56 +0200

    Add jira mock function

--- lua/jira.lua
+++ lua/jira.lua
@@ -37,31 +37,6 @@ local function get_jira_tickets(token, domain)
   end
 end
 
--- This function shows tickets
-local function show_tickets(tickets)
-  local lines = {}
-
-  for _, ticket in ipairs(tickets.issues) do
-    table.insert(lines, ticket.fields.summary)
-  end
-
-  local bufnr = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-
-  local winnr, bufnr = popup.create(bufnr, {
-    title = "Jira Tickets",
-    highlight = "Normal",
-    border = "rounded",
-    width = 60,
-    height = 20,
-    row = 5,
-    col = 5,
-  })
-
-  -- Add mappings to close the window
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "q", ":close<CR>", { silent = true, nowait = true, noremap = true })
-end
-
 -- Mock function to simulate getting Jira tickets
 local function mock_get_jira_tickets(token, domain)
   -- Mock data
@@ -105,6 +80,31 @@ local function mock_get_jira_tickets(token, domain)
   end
 end
 
+-- This function shows tickets
+local function show_tickets(tickets)
+  local lines = {}
+
+  for _, ticket in ipairs(tickets.issues) do
+    table.insert(lines, ticket.fields.summary)
+  end
+
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+
+  local winnr, bufnr = popup.create(bufnr, {
+    title = "Jira Tickets",
+    highlight = "Normal",
+    border = "rounded",
+    width = 60,
+    height = 20,
+    row = 5,
+    col = 5,
+  })
+
+  -- Add mappings to close the window
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "q", ":close<CR>", { silent = true, nowait = true, noremap = true })
+end
+
 -- Execute the plugin
 local function execute(config)
   vim.api.nvim_create_user_command("Jira", function(opts)
@@ -112,11 +112,10 @@ local function execute(config)
     local domain = config.domain
 
     if not token or not domain then
-      vim.notify("Please set Jira token and domain in your Neovim config", vim.log.levels.ERROR)
-      return
+      local tickets = mock_get_jira_tickets(token, domain)
+    else
+      local tickets = get_jira_tickets(token, domain)
     end
-
-    local tickets = mock_get_jira_tickets(token, domain)
 
     if next(tickets) ~= nil then
       show_tickets(tickets)
